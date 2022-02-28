422a4a38a33a93dca2e2eaedaa597c0587d8fed1
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-02-28 00:05:52 -0300

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyding",
-    version="1.3.0",
+    version="1.3.1",
     author = 'João Lucas Bernardi',
     author_email="joao_bernardi@outlook.com",
     description="Simpĺe but effective event framework",
