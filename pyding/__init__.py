023dc0763f19b6e14f2fd44fd128391bfaaaaac2
Jão <joao_bernardi@outlook.com>
2022-08-10 08:03:38 -0300

    `Methods` - Proxy methods from global event space

--- pyding/__init__.py
+++ pyding/__init__.py
@@ -1,7 +1,7 @@
-from .methods import on, call
+from .methods import on, call, handlers_registered, register_handler, unregister_from_module, unregister_handler
 from .structures import EventCall, EventHandler, EventSupport
 from .exceptions import UncancellableEvent
 from .event_space import EventSpace
 
 __name__ = "pyding"
-__version__ = "1.5.1"
+__version__ = "1.5.2"
