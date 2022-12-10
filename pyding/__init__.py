5e54233b8095baaee08866ca3ef29d9eeb187798
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-12-10 14:19:17 -0300

    `Version bump`

--- pyding/__init__.py
+++ pyding/__init__.py
@@ -1,7 +1,7 @@
-from .methods import on, call, async_call, handlers_registered, register_handler, unregister_from_module, unregister_handler
+from .methods import wait_for, on, call, async_call, handlers_registered, register_handler, unregister_from_module, unregister_handler
 from .structures import EventCall, EventHandler, EventSupport
 from .exceptions import UncancellableEvent
 from .event_space import EventSpace
 
 __name__ = "pyding"
-__version__ = "1.6.2.1"
+__version__ = "1.7.0"
